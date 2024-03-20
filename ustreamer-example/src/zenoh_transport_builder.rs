b6deabb27ae3447e8f5a2ca679a18c5bbf800573
Peter LeVasseur <peter.levasseur@gm.com>
2024-03-20 14:23:26 -0400

    Doing some moving around and shuffling to restart with feedback on pluggable streamer concept.

--- ustreamer-example/src/zenoh_transport_builder.rs
+++ ustreamer-example/src/zenoh_transport_builder.rs
@@ -1,33 +1,33 @@
-use async_std::task;
-use up_client_zenoh::UPClientZenoh;
-use up_rust::transport::datamodel::UTransport;
-use ustreamer::utransport_builder::UTransportBuilder;
-use zenoh::prelude::Config;
-
-pub struct ZenohTransportBuilder {
-    config: Option<Config>,
-}
-
-impl ZenohTransportBuilder {
-    #[allow(dead_code)]
-    pub fn new_with_config(config: Config) -> Self {
-        Self {
-            config: Some(config),
-        }
-    }
-
-    #[allow(dead_code)]
-    pub fn new() -> Self {
-        Self { config: None }
-    }
-}
-
-impl UTransportBuilder for ZenohTransportBuilder {
-    fn build(&self) -> Box<dyn UTransport> {
-        let config = self.config.clone().unwrap_or_default();
-        let utransport: Box<dyn UTransport> = Box::new(
-            task::block_on(UPClientZenoh::new(config)).expect("Unable to create UPClientZenoh!"),
-        );
-        utransport
-    }
-}
+// use async_std::task;
+// use up_client_zenoh::UPClientZenoh;
+// use up_rust::transport::datamodel::UTransport;
+// use ustreamer-previous-1::utransport_builder::UTransportBuilder;
+// use zenoh::prelude::Config;
+//
+// pub struct ZenohTransportBuilder {
+//     config: Option<Config>,
+// }
+//
+// impl ZenohTransportBuilder {
+//     #[allow(dead_code)]
+//     pub fn new_with_config(config: Config) -> Self {
+//         Self {
+//             config: Some(config),
+//         }
+//     }
+//
+//     #[allow(dead_code)]
+//     pub fn new() -> Self {
+//         Self { config: None }
+//     }
+// }
+//
+// impl UTransportBuilder for ZenohTransportBuilder {
+//     fn build(&self) -> Box<dyn UTransport> {
+//         let config = self.config.clone().unwrap_or_default();
+//         let utransport: Box<dyn UTransport> = Box::new(
+//             task::block_on(UPClientZenoh::new(config)).expect("Unable to create UPClientZenoh!"),
+//         );
+//         utransport
+//     }
+// }
