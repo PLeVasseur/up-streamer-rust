9a48a9ba48fba02c5ded8872ae5d63aab2334c53
Peter LeVasseur <peter.levasseur@gm.com>
2024-03-21 14:06:54 -0400

    removing old ustreamer stuff

--- ustreamer-previous-1/src/router.rs
+++
@@ -1,8 +0,0 @@
-use std::error::Error;
-
-pub trait Router: Sized + 'static {
-    type StartArgs;
-    type Instance;
-    /// Starts your router. Use `Ok` to return your router's control structure
-    fn start(name: &str, args: &Self::StartArgs) -> Result<Self::Instance, Box<dyn Error>>;
-}
--- ustreamer-previous-1/src/utransport_builder.rs
+++
@@ -1,5 +0,0 @@
-use up_rust::transport::datamodel::UTransport;
-
-pub trait UTransportBuilder: Send + Sync {
-    fn build(&self) -> Box<dyn UTransport>;
-}
