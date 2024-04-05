c6c20dd36dc9a927f8cbb9e25a616ffe92b9df6d
Peter LeVasseur <peter.levasseur@gm.com>
2024-04-04 23:20:45 -0400

    Needed to vastly bump up the size of the broadcast channel to stress test

--- examples/simple-example/src/main.rs
+++ examples/simple-example/src/main.rs
@@ -21,7 +21,7 @@ use log::{debug, error};
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::thread::JoinHandle;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use up_rust::UMessageType::{
     UMESSAGE_TYPE_NOTIFICATION, UMESSAGE_TYPE_PUBLISH, UMESSAGE_TYPE_REQUEST,
     UMESSAGE_TYPE_RESPONSE,
@@ -37,8 +37,8 @@ static FINISH_CLIENTS: AtomicBool = AtomicBool::new(false);
 #[async_std::main]
 async fn main() {
     // using async_broadcast to simulate communication protocol
-    let (tx_1, rx_1) = broadcast(100);
-    let (tx_2, rx_2) = broadcast(100);
+    let (tx_1, rx_1) = broadcast(8000);
+    let (tx_2, rx_2) = broadcast(8000);
 
     let utransport_foo: Arc<Mutex<Box<dyn UTransport>>> = Arc::new(Mutex::new(Box::new(
         UPClientFoo::new("upclient_foo", rx_1.clone(), tx_1.clone()).await,
@@ -105,15 +105,6 @@ async fn main() {
     task::sleep(Duration::from_millis(1000)).await;
 
     debug!("Finished!");
-
-    FINISH_CLIENTS.store(true, Ordering::SeqCst);
-
-    debug!("set FINISH_CLIENTS to true");
-
-    debug!(
-        "FINISH_CLIENTS.load(): {}",
-        FINISH_CLIENTS.load(Ordering::SeqCst)
-    );
 
     let recv_1 = handle_1.join().expect("Unable to join on handle_1");
     let recv_2 = handle_2.join().expect("Unable to join on handle_2");
@@ -330,21 +321,20 @@ pub async fn run_client(
             //     panic!("Unable to register!");
             // };
 
+            let start = Instant::now();
+
             loop {
-                debug!("top of loop");
+                println!("top of loop");
 
-                // task::yield_now().await;
+                let current = Instant::now();
+                let ellapsed = current - start;
 
-                task::sleep(Duration::from_millis(50)).await;
+                println!("ellapsed: {}", ellapsed.as_millis());
 
-                if FINISH_CLIENTS.load(Ordering::SeqCst) {
-                    debug!("Received a return request, performing the action...");
-                    // Handle the return request
+                if ellapsed.as_millis() > 1000 {
                     let times: u64 = client.times_received.load(Ordering::Relaxed);
                     println!("{name} had rx of: {times}");
                     return times;
-                } else {
-                    debug!("No request to quit yet");
                 }
 
                 debug!("-----------------------------------------------------------------------");
@@ -402,6 +392,13 @@ pub async fn run_client(
                     "prior to sending from client {}, the response message: {:?}",
                     &name, &response_msg
                 );
+
+                let send_res = client.send(response_msg).await;
+                if send_res.is_err() {
+                    error!("Unable to send from client: {}", &name);
+                }
+
+                task::sleep(Duration::from_millis(1)).await;
             }
         });
         res
