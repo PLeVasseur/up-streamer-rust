72cc34fdeed7e70ff1a32a4150656b0727b7e661
Peter LeVasseur <peter.levasseur@gm.com>
2024-06-25 08:36:07 -0400

    Simplified down to an easily buildable version that could be tested without vsomeip. Showed that the way the example plugin worked seems to not work here.

--- up-linux-streamer-plugin/src/lib.rs
+++ up-linux-streamer-plugin/src/lib.rs
@@ -13,12 +13,7 @@ use std::sync::{
     Arc, Mutex,
 };
 use std::time::Duration;
-use tracing::{debug, info};
-use tracing::{error, trace};
-use up_rust::UTransport;
-use up_streamer::{Endpoint, UStreamer};
-use up_transport_vsomeip::UPTransportVsomeip;
-use up_transport_zenoh::UPClientZenoh;
+use tracing::{debug, error, info, trace};
 use zenoh::plugins::{RunningPluginTrait, ZenohPlugin};
 use zenoh::prelude::r#async::*;
 use zenoh::runtime::Runtime;
@@ -52,102 +47,12 @@ impl Plugin for ExamplePlugin {
     fn start(name: &str, runtime: &Self::StartArgs) -> ZResult<Self::Instance> {
         zenoh_util::try_init_log_from_env();
         trace!("up-linux-streamer-plugin: start");
-        let mut config = runtime.config().clone();
-        let config_keys = config.keys();
         trace!("name: {name}");
-        trace!("config_keys: {config_keys:?}");
-        {
-            let plugins = config.get("plugins");
-            match plugins {
-                Ok(config_guard) => {
-                    // @CY / @Luca - Looks like this is None for some reason?
-                    trace!("config_guard: {:?}", config_guard.downcast_ref::<Config>());
-                }
-                Err(err) => {
-                    error!("Unable to get plugins: {err:?}");
-                }
-            }
-            trace!("Made past match on plugins");
-        }
-
-        // @CY / @Luca -- seems to crash here?
-        // let config_plugin = runtime.config().get_json("plugins");
-
-        trace!("Made past reading plugins key");
-
-        {
-            let config = config.lock();
-            let plugins_config = config.plugin(name);
-
-            trace!("plugins_config: {plugins_config:?}");
-        }
-
-        // {
-        //     let config = config.lock();
-        //     let some_read = config.plugins_loading();
-        //     trace!("some_read: {some_read:?}");
-        // }
-
-        trace!("Before attempting to read config");
-        {
-            // @CY / Luca -- this bit was lifted from the zenoh-plugin-storage-manager and seems
-            //  to also crash due to runtime.config().lock().plugin(name) being None
-            // let config =
-            //     { PluginConfig::try_from((name, runtime.config().lock().plugin(name).unwrap())) }?;
-        }
-        trace!("After attempting to read config");
-
-        {
-            let config_guard = config.lock();
-            // @CY / @Luca -- this appears to panic
-            // trace!("config_guard: {:?}", config_guard.to_string());
-            let plugin_config = config_guard.plugin(name);
-            // @CY / @Luca - Looks like this is None for some reason?
-            trace!("maybe_config: {plugin_config:?}");
-            if let Some(config) = plugin_config {
-                let maybe_config_object = config.as_object();
-                trace!("maybe_config_object: {maybe_config_object:?}");
-            }
-        }
-
-        // @CY / Luca -- unable to read this, we can confirm that it does exist from reading
-        //  the initial loading of the plugin
-        trace!("Trying to read specific portion of config");
-        {
-            let config_guard = config.lock();
-            let baz = config_guard.get_json("plugins/up_linux_streamer/__required__");
-
-            trace!("__required__: {baz:?}");
-        }
-        trace!("Able to read specific portion of config");
-
-        // @CY / Luca -- this appears to also not work
-        // trace!("Trying to read plugins_loading");
-        // {
-        //     let config_guard = config.lock();
-        //     if let Some(ref search_dirs) = config_guard.plugins_loading.search_dirs {
-        //         trace!("search_dirs: {search_dirs:?}");
-        //     }
-        // }
-        // trace!("After plugins_loading");
-
-        // @CY / Luca -- this crashes if we attempt to read .plugins()
-        // trace!("Before attempting to read plugins_config");
-        // {
-        //     let config_guard = config.lock();
-        //     let plugins_config = config_guard.plugins();
-        //     trace!("plugins_config: {plugins_config:?}");
-        // }
-        // trace!("After attempting to read plugins_config");
-
-        // This also doesn't appear to work
-        // trace!("Attempting to insert serialized value into config");
-        // {
-        //     let val: u32 = 10;
-        //     let serialized_val = serde_json::to_value(val).expect("Failed to serialize val");
-        //     config.insert::<serde_json::Value>("plugins", serialized_val.into()).expect("Unable to add abc as a key");
-        // }
-        // trace!("After inserting serialized value into config");
+        // @CY / Luca -- Doing the same as in the example plugin here leads to a runtime panic
+        //  due to config.plugin(name) being None
+        let config = runtime.config().lock();
+        let self_cfg = config.plugin(name).unwrap().as_object().unwrap();
+        trace!("self_cfg: {self_cfg:?}");
 
         let selector = KeyExpr::try_from(DEFAULT_SELECTOR).unwrap();
 
@@ -247,81 +152,6 @@ async fn run(runtime: Runtime, selector: KeyExpr<'_>, flag: Arc<AtomicBool>) {
     trace!("called function on runtime: {timestamp_res:?}");
 
     env_logger::init();
-
-    let mut streamer = UStreamer::new("up-linux-streamer", 10000);
-
-    let exe_path = match env::current_exe() {
-        Ok(exe_path) => {
-            if let Some(exe_dir) = exe_path.parent() {
-                println!("The binary is located in: {}", exe_dir.display());
-                exe_path
-            } else {
-                panic!("Failed to determine the directory of the executable.");
-            }
-        }
-        Err(e) => {
-            panic!("Failed to get the executable path: {}", e);
-        }
-    };
-    tracing::log::trace!("exe_path: {exe_path:?}");
-    let exe_path_parent = exe_path.parent();
-    let Some(exe_path_parent) = exe_path_parent else {
-        panic!("Unable to get parent path");
-    };
-    tracing::log::trace!("exe_path_parent: {exe_path_parent:?}");
-
-    // TODO: Make configurable to pass the path to the vsomeip config as a command line argument
-    let vsomeip_config = PathBuf::from(exe_path_parent).join("vsomeip-configs/point_to_point.json");
-    tracing::log::trace!("vsomeip_config: {vsomeip_config:?}");
-    let vsomeip_config = canonicalize(vsomeip_config).ok();
-    tracing::log::trace!("vsomeip_config: {vsomeip_config:?}");
-
-    // There will be a single vsomeip_transport, as there is a connection into device and a streamer
-    // TODO: Add error handling if we fail to create a UPTransportVsomeip
-    let vsomeip_transport: Arc<dyn UTransport> = Arc::new(
-        UPTransportVsomeip::new_with_config(&"linux".to_string(), 10, &vsomeip_config.unwrap())
-            .unwrap(),
-    );
-
-    // TODO: Probably make somewhat configurable?
-    let zenoh_config = Config::default();
-    // TODO: Add error handling if we fail to create a UPClientZenoh
-    let zenoh_transport: Arc<dyn UTransport> = Arc::new(
-        UPClientZenoh::new_with_runtime(runtime.clone(), "linux".to_string())
-            .await
-            .unwrap(),
-    );
-    // TODO: Make configurable to pass the name of the mE authority as a  command line argument
-    let vsomeip_endpoint = Endpoint::new(
-        "vsomeip_endpoint",
-        "me_authority",
-        vsomeip_transport.clone(),
-    );
-
-    // TODO: Make configurable the ability to have perhaps a config file we pass in that has all the
-    //  relevant authorities over Zenoh that should be forwarded
-    let zenoh_transport_endpoint_a = Endpoint::new(
-        "zenoh_transport_endpoint_a",
-        "linux", // simple initial case of streamer + intended high compute destination on same device
-        zenoh_transport.clone(),
-    );
-
-    // TODO: Per Zenoh endpoint configured, run these two rules
-    let forwarding_res = streamer
-        .add_forwarding_rule(vsomeip_endpoint.clone(), zenoh_transport_endpoint_a.clone())
-        .await;
-
-    if let Err(err) = forwarding_res {
-        error!("Unable to add forwarding result: {err:?}");
-    }
-
-    let forwarding_res = streamer
-        .add_forwarding_rule(zenoh_transport_endpoint_a.clone(), vsomeip_endpoint.clone())
-        .await;
-
-    if let Err(err) = forwarding_res {
-        error!("Unable to add forwarding result: {err:?}");
-    }
 
     // Plugin's event loop, while the flag is true
     let mut counter = 1;
